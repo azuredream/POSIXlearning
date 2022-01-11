b31fb0bfab0cfba50d25e1ead20cfbc7dd5806b6
azuredream <zhaozixuan67@gmail.com>
2022-01-10 23:44:49 -0500

    Add reousrce history to HistoryKeepe. New class IndexNavr

--- python/autocomplete.py
+++ python/autocomplete.py
@@ -10,9 +10,11 @@ from typing import Optional
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter, NestedCompleter, Completer, Completion
 
+
+
 ncompleter = NestedCompleter.from_nested_dict({
-    'symdev': {
-        '-sid': {
+    'device': {
+        '295': {
             'create': {
                 '-num': None
             },
@@ -24,14 +26,25 @@ ncompleter = NestedCompleter.from_nested_dict({
             },
         },
     },
-    'symclone': {},
-    'symsnapvx': {},
-    'symcfg': {},
-    'symcli':{},
 })
 
+class IndexNavigator():
+    def __init__(self, indexdoc) -> None:
+        self.context = "" #string of the index document
+        self.i       = 0  #cursor position in the document
+        with open(indexdoc) as f:
+            self.context = f.readlines()
+
+
+
 class MyCompleter(Completer):
+    def __init__(self, idx_nav) -> None:
+        super().__init__()
+        self.idx_nav = idx_nav
+        
     def get_completions(self, document, complete_event):
+#考虑每次根据doc新建nav还是一行命令用一个nav
+
         for i in range(5):
             yield Completion('completion' + str(math.floor(random.random()*10)), start_position=0)
 
@@ -39,14 +52,32 @@ class MyCompleter(Completer):
 #OOP version of global list history[]
 class HistoryKeeper():
     def __init__(self) -> None:
-        self.history = []
+        self.cmdhistory = []
+        self.svrhistory = []
+        self.devhistory = []
 
-    def put_history(self, str):
-        self.history.append(str)
+    def put_reshistory(self, res_type, s):
+        if res_type == "svr":
+            self.svrhistory.append(s)
+        elif res_type == "dev":
+            self.devhistory.append(s)
+        else:
+            print("error: res_type not support.")
+    
+    def get_reshistory(self, res_type):
+        if res_type == "svr":
+            return self.svrhistory
+        elif res_type == "dev":
+            return self.devhistory
+        else:
+            print("error: res_type not support.")
+
+    def put_history(self, cmd):
+        self.cmdhistory.append(cmd)
 
     def get_history(self):
         # get this list from a file or secli session
-        return self.history
+        return self.cmdhistory
 
 class MySuggestion(AutoSuggest):
     def __init__(self, **kwargs) -> None:
@@ -73,8 +104,9 @@ class MySuggestion(AutoSuggest):
 
 def main():
     historykeeper = HistoryKeeper()
+    indexnav      = IndexNavigator("./doc/dev.ix")
     while True:
-        text = prompt('> ', completer=MyCompleter(), auto_suggest=MySuggestion(historykeeper = historykeeper))
+        text = prompt('> ', completer=MyCompleter(indexnav), auto_suggest=MySuggestion(historykeeper = historykeeper))
         historykeeper.put_history(text)
         if (text == "quit"):
             print("cli exit...")
